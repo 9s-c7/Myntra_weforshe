ab74715df3f3c1072b1dce1e505eae810bdbd3f2
Sonal <123151672+9s-c7@users.noreply.github.com>
2024-07-11 14:46:39 +0530

    Add files via upload

---
+++ components/imagebar/Imagebarstyles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    marginBottom:5,
+  },
+  scrollContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  imageContainer: {
+    marginRight: 10,
+  },
+  image: {
+    width: 80,
+    height: 110,
+  },
+});
