c20dd95db06ee367abef6e4481d4dd41567d6448
Sonal <123151672+9s-c7@users.noreply.github.com>
2024-07-11 14:47:02 +0530

    Add files via upload

---
+++ screens/Homestyles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  Barcontainer: {
+      display:'flex',
+      flexDirection:'column',
+      gap:0,
+    },
+  });
