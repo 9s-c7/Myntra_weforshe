3e3acb0f1b7944d60f0f0bdf51c520290281d33b
Sonal <123151672+9s-c7@users.noreply.github.com>
2024-07-11 14:46:52 +0530

    Delete screens directory

--- screens/Homestyles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const styles = StyleSheet.create({
-  Barcontainer: {
-      display:'flex',
-      flexDirection:'column',
-      gap:0,
-    },
-  });
